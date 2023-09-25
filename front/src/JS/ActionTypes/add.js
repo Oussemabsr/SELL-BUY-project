5c9db06516ca5ea4605d85ab6ad93b59b3137341
unknown <oussemabsr874@gmail.com>
2023-09-25 15:57:13 +0100

    Remove cached front directory

--- front/src/JS/ActionTypes/add.js
+++
@@ -1,5 +0,0 @@
-export const ADD_POST = 'ADD_POST';
-export const EDIT_POST = 'EDIT_POST';
-export const DELETE_POST = 'DELETE_POST';
-export const FETCH_POSTS_SUCCESS = 'FETCH_POSTS_SUCCESS'; 
-export const FETCH_POSTS_FAILURE = 'FETCH_POSTS_FAILURE';
